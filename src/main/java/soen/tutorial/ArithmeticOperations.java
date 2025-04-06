af8fbe95d6ce2a3c4b9bc139fef4eced494948c5
AyeshaMah01 <158636557+AyeshaMah01@users.noreply.github.com>
2025-04-06 06:40:58 -0400

    Update ArithmeticOperations.java

--- src/main/java/soen/tutorial/ArithmeticOperations.java
+++ src/main/java/soen/tutorial/ArithmeticOperations.java
@@ -11,9 +11,5 @@ public class ArithmeticOperations {
 		}
 	}
 	
-	public Integer multiply(int a, int b) {
-        // Side effect to prevent optimization
-        System.out.print("");  // ensures method is kept
-        return a * b;
-    }
+	
 }
